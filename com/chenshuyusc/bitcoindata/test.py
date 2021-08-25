ca348d010a8c5df5dd125a833ddca264fc58f92c
kai wang <20110240034@fudan.edu.cn>
2021-08-25 15:50:30 +0800

    C++

--- com/chenshuyusc/bitcoindata/test.py
+++
@@ -1,8 +0,0 @@
-from com.chenshuyusc.bitcoindata.DoubleLinked3 import DoubleLinked
-
-if __name__ == '__main__':
-    dl = DoubleLinked('/Users/chenshuyu/Documents/blockChain/BTC_deanonymization_model/data')
-
-    dl.deluxto()
-    # dl.getYearsAndTimes()
-    dl.doublelinked()
--- com/chenshuyusc/process/dan2shuang.py
+++
@@ -1,8 +0,0 @@
-
-
-f = open('/Users/chenshuyu/Documents/blockChain/BTC_deanonymization_model/result.csv', 'r', encoding='UTF-8-sig')
-fw = open('/Users/chenshuyu/Documents/blockChain/BTC_deanonymization_model/result2.csv', 'w')
-new_lines = []
-for line in f:
-    line = line.replace("\'", "\"")
-    fw.write(line)
